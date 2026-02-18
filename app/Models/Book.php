69b80c8e78807e879cf76a45e13b75a89e66c58f
Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2026-02-18 23:03:26 +0530

    HEADS UP! Explaining the Confusion with "Popular Books" Feature!

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,12 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function reviews(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $qurey, string $title): Builder
+    {
+        return $qurey->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopuler(Builder $qurey): Builder
+    {
+        return $qurey->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+
+    public function scopeHeighestRated(Builder $qurey): Builder
+    {
+        return $qurey->withAvg('reviews','rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+
+
+
+
+
 }
