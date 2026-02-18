e977cddcfd16a0308a5c00b60c1b9722f01879e6
Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2026-02-19 00:25:59 +0530

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -15,27 +16,37 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $qurey, string $title): Builder
+     public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $qurey->where('title', 'LIKE', '%' . $title . '%');
-    }
-
-    public function scopePopuler(Builder $qurey): Builder
-    {
-        return $qurey->withCount('reviews')
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
             ->orderBy('reviews_count', 'desc');
     }
 
-
-    public function scopeHeighestRated(Builder $qurey): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $qurey->withAvg('reviews','rating')
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
 
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+         return $query->having('reviews_count', '>=', $minReviews);
+    }
 
-
-
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
 
 }
